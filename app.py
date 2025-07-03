63504041853926a39f6e057b3052ec656f366156
ryanchan <ryanchan339@gmail.com>
2025-07-03 16:27:10 -0700

    dashboard template setup with boarding pass

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import json
 app = Flask(__name__)
 app.secret_key = "apple"
 
-
-
 @app.route("/dashboard")
 def dashboard():
     return "<h1>Welcome to your Travel Planner Dashboard!</h1>"
@@ -28,10 +26,10 @@ def login():
         if username in users:
             if password == users[username]["password"]:
                 flash("Login Successful")
+                return render_template("dashboard.html")
         else:
             flash("Invalid Username or Password")
-
-    return render_template("login.html")
+            return render_template("login.html")
 
 @app.route("/go_create", methods=["GET", "POST"])
 def go_create():
@@ -43,20 +41,20 @@ def create_account():
         username = request.form["username"]
         password = request.form["password"]
 
-    with open("users.json", "r") as file:
-        user_list = json.load(file)
+        with open("users.json", "r") as file:
+            user_list = json.load(file)
 
-    if username in user_list:
-        flash("Username already exists")
-        return render_template("create.html")
+        if username in user_list:
+            flash("Username already exists")
+            return render_template("create.html")
 
-    user_list[username] = {"password": password}
+        user_list[username] = {"password": password}
 
-    with open("users.json", "w") as file:
-        json.dump(user_list, file)
+        with open("users.json", "w") as file:
+            json.dump(user_list, file)
 
-    flash("Account created successfully")
-    return render_template("login.html")  # or redirect to dashboard    return render_template("login.html") 
+        flash("Account created successfully")
+        return render_template("dashboard.html")
         
 
 if __name__ == "__main__":
